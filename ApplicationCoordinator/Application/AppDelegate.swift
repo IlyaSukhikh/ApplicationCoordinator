cf2afa0df9f3327fff7fe69926f33883159952a2
Andrey Panov <panovdev@gmail.com>
2016-05-31 22:26:08 +0300

    local notification add

--- ApplicationCoordinator/Application/AppDelegate.swift
+++ ApplicationCoordinator/Application/AppDelegate.swift
@@ -21,7 +21,38 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
         applicationCoordinator.start()
+        registerNotification()
         return true
     }
+    
+    func scheduleNotification() {
+        
+        if UIApplication.sharedApplication().scheduledLocalNotifications?.count == 0 {
+            
+            let notification = UILocalNotification()
+            notification.alertBody = "Hey! Update your counter ;)"
+            notification.fireDate = NSDate()
+            notification.category = "wef"
+            notification.repeatInterval = NSCalendarUnit.Minute
+            
+            UIApplication.sharedApplication().scheduleLocalNotification(notification)
+        }
+    }
+    
+    func application(application: UIApplication, didRegisterUserNotificationSettings notificationSettings: UIUserNotificationSettings) {
+        scheduleNotification()
+    }
+    
+    // Register notification settings
+    func registerNotification() {
+        
+        let counterCategory = UIMutableUserNotificationCategory()
+        counterCategory.identifier = "sfwef"
+        
+        let types: UIUserNotificationType = [UIUserNotificationType.Alert, UIUserNotificationType.Sound]
+        let settings = UIUserNotificationSettings(forTypes: types, categories: NSSet(object: counterCategory) as? Set<UIUserNotificationCategory>)
+        UIApplication.sharedApplication().registerUserNotificationSettings(settings)
+    }
+
 }
 
